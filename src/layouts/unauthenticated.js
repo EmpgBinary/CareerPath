98ec9c0d7189a0e3e67dee487b9656860f3a98a6
Codam <damisco005@gmail.com>
2024-04-22 16:44:27 +0100

    responsive unauthenticated layout

--- src/layouts/unauthenticated.js
+++ src/layouts/unauthenticated.js
@@ -8,8 +8,8 @@ export default function Unauthenticated() {
       <div className="h-full lg:w-1/2">
         <Outlet />
       </div>
-      <div className="w-full h-full lg:w-1/2 bg-white-500 flex justify-between items-center p-6">
-        <img src={Path} className="w-full h-full" object-cover alt=""/>
+      <div className="w-full h-full lg:w-1/2 bg-white-500 md:flex justify-between items-center p-6 hidden">
+        <img src={Path} className="w-full h-full" object-cover alt="" />
       </div>
     </div>
   );
